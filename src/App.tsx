800d7be9f2010b9ca075368a98596d9e88077741
Carmelo Mayen <carmelo5mayen@gmail.com>
2025-01-15 19:17:59 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { menuItems } from './data/db'
+
+
+function App() {
+
+
+
+  return (
+    <>
+      <header className='bg-teal-400 py-5'>
+        <h1 className='text-center text-4xl font-black'>Tip and order Calculator</h1>
+      </header>
+
+      <main className='max-w-7xl mx-auto py-20'>
+        <h2>Menu</h2>
+        <h2>Orders</h2>
+      </main>
+    </>
+  )
+}
+
+export default App
